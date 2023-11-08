e08e99f3db03090f3f3e59c2af071829ba3e50d4
minah <jma3620@gmail.com>
2023-11-08 14:50:48 +0900

    merging을 위한 backup

--- src/main/java/com/matzalal/web/controller/SearchController.java
+++ src/main/java/com/matzalal/web/controller/SearchController.java
@@ -1,6 +1,8 @@
 package com.matzalal.web.controller;
 
+import com.matzalal.web.entity.Rest;
 import com.matzalal.web.entity.RestView;
+import com.matzalal.web.service.RestService;
 import com.matzalal.web.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,14 +18,20 @@ public class SearchController {
 
     @Autowired
     private SearchService service;
+    
+    @Autowired
+    private RestService restService;
 
     @RequestMapping("result")
-    public String list(Model model,
-                       @RequestParam(required = false) String query){
-        List<RestView> list = service.getViewList(query);
-        System.out.println(query);
+    public String list(Model model
+//                       @RequestParam(required = false) String query
+                       ){
+        //List<RestView> list = service.getViewList(query);
+    	
+    	List<Rest> list = restService.getList();
+//        System.out.println(query);
         System.out.println(list);
         model.addAttribute("list", list);
-        return "/search/search_result";
+        return "search/search_result";
     }
 }
